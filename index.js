75d525c8351895360d44463b800df0105c5dc0d6
Sophie Gu <sophieqgu@gmail.com>
2021-01-03 16:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ function leashDog(dogName, dogBreed) {
 function walkToPark(dogName, dogBreed) {
   console.log(`Walk ${dogName} the ${dogBreed} to Park`);
 }
+
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Walk ${dogName} the ${dogBreed} to Park`);
+}
+
+function walkHome(dogName, dogBreed) {
+  console.log(`Walk ${dogName} the ${dogBreed} home`);
+}
+
+function unleashDog(dogName, dogBreed) {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+}
