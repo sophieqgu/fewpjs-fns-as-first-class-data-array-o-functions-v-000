087167c8e6977cc2d75e27e1b45b1ba8a083ec33
Sophie Gu <sophieqgu@gmail.com>
2021-01-03 16:15:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+
+function leashDog(dogName, dogBreed) {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName, dogBreed) {
+  console.log(`Walk ${dogName} the ${dogBreed} to Park`);
+}
